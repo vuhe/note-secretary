446939466064c0b791dcbe74071fb2d9629f8417
vuhe <zhuhe202@qq.com>
2025-12-27 06:55:40 +0800

    rust edition 提升为 2024

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,12 +1,44 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 use std::time::{SystemTime, UNIX_EPOCH};
-use tauri::{TitleBarStyle, WebviewUrl, WebviewWindowBuilder};
+use tauri::{Result, TitleBarStyle, WebviewUrl, WebviewWindow, WebviewWindowBuilder};
 
 #[tauri::command]
 fn greet() -> String {
   let now = SystemTime::now();
   let epoch_ms = now.duration_since(UNIX_EPOCH).unwrap().as_millis();
   format!("Hello world from Rust! Current epoch: {epoch_ms}")
+}
+
+#[cfg(target_os = "macos")]
+fn set_macos_title_bar(window: &WebviewWindow) -> Result<()> {
+  use block2::RcBlock;
+  use objc2::rc::Retained;
+  use objc2_app_kit::{NSAppearance, NSAppearanceNameDarkAqua, NSColor, NSWindow};
+  use std::ptr::NonNull;
+
+  let ns_window = unsafe { &*(window.ns_window()? as *const NSWindow) };
+
+  let provider = RcBlock::new(
+    move |appearance: NonNull<NSAppearance>| -> NonNull<NSColor> {
+      let app = unsafe { appearance.as_ref() };
+
+      // 检查当前外观名称是否为深色 (DarkAqua)
+      let color = if unsafe { *app.name() == *NSAppearanceNameDarkAqua } {
+        // 返回深色模式下的颜色
+        NSColor::colorWithRed_green_blue_alpha(23.0 / 255.0, 23.0 / 255.0, 23.0 / 255.0, 1.0)
+      } else {
+        // 返回浅色模式下的颜色
+        NSColor::colorWithRed_green_blue_alpha(250.0 / 255.0, 250.0 / 255.0, 250.0 / 255.0, 1.0)
+      };
+
+      unsafe { NonNull::new_unchecked(Retained::as_ptr(&color) as *mut NSColor) }
+    },
+  );
+
+  let dynamic_color = unsafe { NSColor::colorWithName_dynamicProvider(None, &provider) };
+  ns_window.setBackgroundColor(Some(&dynamic_color));
+
+  Ok(())
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -25,53 +57,11 @@ pub fn run() {
       #[cfg(target_os = "macos")]
       let win_builder = win_builder.title_bar_style(TitleBarStyle::Transparent);
 
-      let window = win_builder.build().unwrap();
+      let window = win_builder.build()?;
 
       // 仅在构建 macOS 时设置背景颜色
       #[cfg(target_os = "macos")]
-      {
-        use block2::RcBlock;
-        use objc2::rc::Retained;
-        use objc2_app_kit::{NSAppearance, NSAppearanceNameDarkAqua, NSColor, NSWindow};
-        use std::ptr::NonNull;
-
-        unsafe {
-          let ns_window = &*(window.ns_window().unwrap() as *const NSWindow);
-
-          let provider = RcBlock::new(
-            move |appearance: NonNull<NSAppearance>| -> NonNull<NSColor> {
-              let app = appearance.as_ref();
-
-              // 检查当前外观名称是否为深色 (DarkAqua)
-              // bestMatchFromAppearances 会在数组中寻找最匹配的外观
-              let is_dark = *app.name() == *NSAppearanceNameDarkAqua;
-
-              let color = if is_dark {
-                // 返回深色模式下的颜色
-                NSColor::colorWithRed_green_blue_alpha(
-                  23.0 / 255.0,
-                  23.0 / 255.0,
-                  23.0 / 255.0,
-                  1.0,
-                )
-              } else {
-                // 返回浅色模式下的颜色
-                NSColor::colorWithRed_green_blue_alpha(
-                  250.0 / 255.0,
-                  250.0 / 255.0,
-                  250.0 / 255.0,
-                  1.0,
-                )
-              };
-
-              NonNull::new_unchecked(Retained::as_ptr(&color) as *mut NSColor)
-            },
-          );
-
-          let dynamic_color = NSColor::colorWithName_dynamicProvider(None, &provider);
-          ns_window.setBackgroundColor(Some(&dynamic_color));
-        }
-      }
+      set_macos_title_bar(&window)?;
 
       Ok(())
     })
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,4 +1,5 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+// 用于在 Windows 生产版本（release）中禁用额外的控制台窗口
+// 请勿删除, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
