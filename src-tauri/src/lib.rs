8e3539a52900cdb146a494fb955b8f617104c241
vuhe <zhuhe202@qq.com>
2025-12-29 17:33:35 +0800

    添加全局数据读写目录

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,13 +1,18 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-use std::time::{SystemTime, UNIX_EPOCH};
-use tauri::{Result, TitleBarStyle, WebviewUrl, WebviewWindow, WebviewWindowBuilder};
+use anyhow::anyhow;
+use std::path::PathBuf;
+use std::sync::OnceLock;
+use tauri::{App, Result, WebviewWindow};
 
 #[tauri::command]
 fn greet() -> String {
+  // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+  use std::time::{SystemTime, UNIX_EPOCH};
   let now = SystemTime::now();
   let epoch_ms = now.duration_since(UNIX_EPOCH).unwrap().as_millis();
   format!("Hello world from Rust! Current epoch: {epoch_ms}")
 }
+
+static APP_DATA_PATH: OnceLock<PathBuf> = OnceLock::new();
 
 #[cfg(target_os = "macos")]
 fn set_macos_title_bar(window: &WebviewWindow) -> Result<()> {
@@ -41,27 +46,52 @@ fn set_macos_title_bar(window: &WebviewWindow) -> Result<()> {
   Ok(())
 }
 
+#[allow(unused_variables)]
+fn set_work_dir(app: &App) -> Result<()> {
+  let work_dir = {
+    #[cfg(debug_assertions)]
+    {
+      let mut current = std::env::current_dir()?;
+      current.pop();
+      current.join("test-data")
+    }
+    #[cfg(not(debug_assertions))]
+    {
+      use tauri::Manager;
+      app.path().app_local_data_dir()
+    }
+  };
+
+  std::fs::create_dir_all(&work_dir)?;
+  APP_DATA_PATH
+    .set(work_dir)
+    .map_err(|_| anyhow!("failed to set app data path"))?;
+
+  Ok(())
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
   tauri::Builder::default()
     .plugin(tauri_plugin_opener::init())
     .invoke_handler(tauri::generate_handler![greet])
     .setup(|app| {
-      let win_builder = WebviewWindowBuilder::new(app, "main", WebviewUrl::default())
+      let win_builder = tauri::WebviewWindowBuilder::new(app, "main", tauri::WebviewUrl::default())
         .title("Note Secretary")
-        .inner_size(800.0, 600.0)
+        .inner_size(900.0, 700.0)
         .resizable(true)
         .fullscreen(false);
 
-      // 仅在 macOS 时设置透明标题栏
+      // 仅在 macOS 时设置透明标题栏和背景颜色
       #[cfg(target_os = "macos")]
-      let win_builder = win_builder.title_bar_style(TitleBarStyle::Transparent);
+      {
+        let win_builder = win_builder.title_bar_style(tauri::TitleBarStyle::Transparent);
+        let window = win_builder.build()?;
+        set_macos_title_bar(&window)?;
+      }
 
-      let window = win_builder.build()?;
-
-      // 仅在构建 macOS 时设置背景颜色
-      #[cfg(target_os = "macos")]
-      set_macos_title_bar(&window)?;
+      // 设置全局数据目录
+      set_work_dir(&app)?;
 
       Ok(())
     })
